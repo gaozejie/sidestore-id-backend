b2f3bace601166b02b2a3c15238a0b38a8c28e1f
Fabian Thies <git@fabian-thies.de>
2024-04-22 20:45:54 +0200

    Update swagger documentation with new description and authentication information

--- src/api/doc.rs
+++ src/api/doc.rs
@@ -1,16 +1,18 @@
-use utoipa::OpenApi;
-use crate::api::auth_controller as Authentication;
+use utoipa::{Modify, OpenApi};
+use utoipa::openapi::security::{AuthorizationCode, Flow, OAuth2, Scopes, SecurityScheme};
+
 use crate::api::app_review_controller as AppReviews;
-use crate::api::ping_controller as Health;
-use crate::api::models::auth as AuthModels;
+use crate::api::auth_controller as Authentication;
 use crate::api::models::app_reviews as AppReviewModels;
-use crate::db::models as DBModels;
+use crate::api::models::auth as AuthModels;
 use crate::api::models::MessageResponse;
+use crate::api::ping_controller as Health;
+use crate::db::models as DBModels;
 use crate::errors::ErrorResponse;
 
 #[derive(OpenApi)]
 #[openapi(
-    info(description = "SideStore ID"),
+    info(title = "SideStore ID"),
     paths(
         Authentication::signup,
         Authentication::login,
@@ -51,5 +53,28 @@ use crate::errors::ErrorResponse;
             AppReviewModels::AppReviewStatus,
         ),
     ),
+    modifiers(&SecurityAddon),
+    tags(
+        
+    ),
 )]
 pub struct ApiDoc;
+
+pub struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "oauth",
+                SecurityScheme::OAuth2(OAuth2::new([
+                    Flow::AuthorizationCode(AuthorizationCode::new(
+                        "/api/auth/oauth2/authorize",
+                        "/api/auth/oauth2/token",
+                        Scopes::from_iter([("full", "full scope")])
+                    )),
+                ]))
+            )
+        }
+    }
+}
